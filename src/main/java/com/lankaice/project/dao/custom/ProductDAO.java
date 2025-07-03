2fa0288401c2f1bf5bf6415ee264acbe9048bc4d
Kalana Methsara <kalanamethsara53@gmail.com>
2025-07-04 00:58:23 +0530

    update

--- src/main/java/com/lankaice/project/dao/custom/ProductDAO.java
+++ src/main/java/com/lankaice/project/dao/custom/ProductDAO.java
@@ -4,7 +4,14 @@ import com.lankaice.project.dao.CrudDAO;
 import com.lankaice.project.entity.Product;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ProductDAO extends CrudDAO<Product> {
     String findIdByName(String productName) throws SQLException, ClassNotFoundException;
+
+    boolean updateProductPrice(String productId, double newPrice) throws SQLException, ClassNotFoundException;
+
+    String findNameById(String productId) throws SQLException, ClassNotFoundException;
+
+    List<String> getAllProductIds() throws SQLException, ClassNotFoundException;
 }
--- src/main/java/com/lankaice/project/dao/custom/impl/ProductDAOImpl.java
+++ src/main/java/com/lankaice/project/dao/custom/impl/ProductDAOImpl.java
@@ -1,9 +1,13 @@
 package com.lankaice.project.dao.custom.impl;
 
 import com.lankaice.project.dao.custom.ProductDAO;
+import com.lankaice.project.dao.util.SQLUtil;
+import com.lankaice.project.dto.ProductDto;
 import com.lankaice.project.entity.Product;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +16,19 @@ public class ProductDAOImpl implements ProductDAO {
 
     @Override
     public List<Product> getAll() throws SQLException, ClassNotFoundException {
-        return List.of();
-    }
+        String sql = "SELECT * FROM Product";
+        ResultSet rs = SQLUtil.execute(sql);
+
+        List<Product> productList = new ArrayList<>();
+        while (rs.next()) {
+            productList.add(new Product(
+                    rs.getString("product_id"),
+                    rs.getString("name"),
+                    rs.getDouble("weight"),
+                    rs.getDouble("price_per_unit")
+            ));
+        }
+        return productList;    }
 
     @Override
     public boolean save(Product product) throws SQLException, ClassNotFoundException {
@@ -32,6 +47,32 @@ public class ProductDAOImpl implements ProductDAO {
 
     @Override
     public String findIdByName(String productName) throws SQLException, ClassNotFoundException {
-        return "";
+        ResultSet rs = SQLUtil.execute("SELECT product_id FROM Product WHERE name = ?", productName);
+        if (rs.next()) {
+            return rs.getString("product_id");
+        }
+        return "";    }
+
+    @Override
+    public boolean updateProductPrice(String productId, double newPrice) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE Product SET price_per_unit = ? WHERE product_id = ?";
+        return SQLUtil.execute(sql, newPrice, productId);    }
+
+    @Override
+    public String findNameById(String productId) throws SQLException, ClassNotFoundException {
+        ResultSet rs = SQLUtil.execute("SELECT name FROM Product WHERE product_id = ?", productId);
+        if (rs.next()) {
+            return rs.getString("name");
+        }
+        return "";    }
+
+    @Override
+    public List<String> getAllProductIds() throws SQLException, ClassNotFoundException {
+        ResultSet rs = SQLUtil.execute("SELECT product_id FROM Product");
+        List<String> list = new ArrayList<>();
+        while (rs.next()) {
+            list.add(rs.getString("product_id"));
+        }
+        return list;
     }
 }
