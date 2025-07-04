2848f67baaeeefa6d0f7386f185d0d935d8ce654
Kalana Methsara <kalanamethsara53@gmail.com>
2025-07-04 12:03:08 +0530

    update

--- src/main/java/com/lankaice/project/dao/custom/TransportDAO.java
+++ src/main/java/com/lankaice/project/dao/custom/TransportDAO.java
@@ -1,7 +1,17 @@
 package com.lankaice.project.dao.custom;
 
 import com.lankaice.project.dao.CrudDAO;
+import com.lankaice.project.dto.TransportDto;
 import com.lankaice.project.entity.Transport;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface TransportDAO extends CrudDAO<Transport> {
+    int getTodayTransportTotal() throws SQLException, ClassNotFoundException;
+
+    List<Transport> getTransportByDate(String date) throws SQLException, ClassNotFoundException;
+
+    String getNextId() throws SQLException, ClassNotFoundException;
+
 }
--- src/main/java/com/lankaice/project/dao/custom/impl/TransportDAOImpl.java
+++ src/main/java/com/lankaice/project/dao/custom/impl/TransportDAOImpl.java
@@ -1,4 +1,119 @@
 package com.lankaice.project.dao.custom.impl;
 
-public class TransportDAOImpl {
+import com.lankaice.project.dao.custom.TransportDAO;
+import com.lankaice.project.dao.util.SQLUtil;
+import com.lankaice.project.entity.Transport;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransportDAOImpl implements TransportDAO {
+    @Override
+    public List<Transport> getAll() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM Transport ORDER BY transport_date DESC";
+        ResultSet rs = SQLUtil.execute(sql);
+        List<Transport> transportList = new ArrayList<>();
+
+        while (rs.next()) {
+            transportList.add(new Transport(
+                    rs.getString("transport_id"),
+                    rs.getString("product_id"),
+                    rs.getString("vehicle_number"),
+                    rs.getString("transport_date"),
+                    rs.getTime("start_time") != null ? rs.getTime("start_time").toLocalTime() : null,
+                    rs.getTime("end_time") != null ? rs.getTime("end_time").toLocalTime() : null,
+                    rs.getInt("quantity"),
+                    rs.getString("location"),
+                    rs.getString("status")
+            ));
+        }
+        return transportList;
+    }
+
+    @Override
+    public boolean save(Transport transport) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO Transport (transport_id, product_id, vehicle_number, transport_date, start_time, end_time, quantity, location, status) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        return SQLUtil.execute(sql,
+                transport.getTransportId(),
+                transport.getProductId(),
+                transport.getVehicleNumber(),
+                transport.getTransportDate(),
+                transport.getDeliveryBeginTime() != null ? Time.valueOf(transport.getDeliveryBeginTime()) : null,
+                transport.getDeliveryEndTime() != null ? Time.valueOf(transport.getDeliveryEndTime()) : null,
+                transport.getQuantity(),
+                transport.getLocation(),
+                transport.getStatus()
+        );
+    }
+
+    @Override
+    public boolean update(Transport transport) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE Transport SET product_id = ?, vehicle_number = ?, transport_date = ?, start_time = ?, end_time = ?, quantity = ?, location = ?, status = ? WHERE transport_id = ?";
+
+        return SQLUtil.execute(sql,
+                transport.getProductId(),
+                transport.getVehicleNumber(),
+                transport.getTransportDate(),
+                transport.getDeliveryBeginTime() != null ? Time.valueOf(transport.getDeliveryBeginTime()) : null,
+                transport.getDeliveryEndTime() != null ? Time.valueOf(transport.getDeliveryEndTime()) : null,
+                transport.getQuantity(),
+                transport.getLocation(),
+                transport.getStatus(),
+                transport.getTransportId()
+        );
+    }
+
+    @Override
+    public boolean delete(String transportId) throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM Transport WHERE transport_id = ?";
+        return SQLUtil.execute(sql, transportId);    }
+
+    @Override
+    public int getTodayTransportTotal() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT SUM(quantity) AS total_quantity FROM Transport WHERE transport_date = CURDATE()";
+        ResultSet rs = SQLUtil.execute(sql);
+        if (rs.next()) {
+            return rs.getInt("total_quantity");
+        }
+        return 0;
+    }
+
+    @Override
+    public List<Transport> getTransportByDate(String date) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM Transport WHERE transport_date = ?";
+        ResultSet rs = SQLUtil.execute(sql, date);
+
+        List<Transport> list = new ArrayList<>();
+        while (rs.next()) {
+            list.add(new Transport(
+                    rs.getString("transport_id"),
+                    rs.getString("product_id"),
+                    rs.getString("vehicle_number"),
+                    rs.getString("transport_date"),
+                    rs.getTime("start_time") != null ? rs.getTime("start_time").toLocalTime() : null,
+                    rs.getTime("end_time") != null ? rs.getTime("end_time").toLocalTime() : null,
+                    rs.getInt("quantity"),
+                    rs.getString("location"),
+                    rs.getString("status")
+            ));
+        }
+        return list;    }
+
+    @Override
+    public String getNextId() throws SQLException, ClassNotFoundException {
+        String prefix = "TP";
+        ResultSet rs = SQLUtil.execute("SELECT transport_id FROM Transport ORDER BY transport_id DESC LIMIT 1");
+
+        if (rs.next()) {
+            String lastId = rs.getString("transport_id");
+            int nextNum = Integer.parseInt(lastId.substring(2)) + 1;
+            return String.format("%s%03d", prefix, nextNum);
+        }
+        return prefix + "001";
+    }
 }
